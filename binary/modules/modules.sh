1ee3b543e5a856ff491e63cfd8053e8be5e89da6
Francis Korning <fkorning@gmail.com>
2026-01-20 10:56:24 +0000

    Update modules.sh

--- binary/modules/modules.sh
+++ binary/modules/modules.sh
@@ -1,6 +1,6 @@
 
 mkdir -p azure/
 
-curl -o azure/terraform-azurerm-avm-res-apimanagement-service_v0.0.6_source.zip https://github.com/Azure/terraform-azurerm-avm-res-apimanagement-service/archive/refs/tags/v0.0.6.zip
+curl -o azure/terraform-azurerm-avm-res-apimanagement-service_v0.0.6.zip https://github.com/Azure/terraform-azurerm-avm-res-apimanagement-service/archive/refs/tags/v0.0.6.zip
 
-curl -o azure/terraform-azurerm-avm-res-network-virtualnetwork_v0.9.2_source.zip https://github.com/Azure/terraform-azurerm-avm-res-network-virtualnetwork/archive/refs/tags/0.9.2.zip
+curl -o azure/terraform-azurerm-avm-res-network-virtualnetwork_v0.9.2.zip https://github.com/Azure/terraform-azurerm-avm-res-network-virtualnetwork/archive/refs/tags/0.9.2.zip
